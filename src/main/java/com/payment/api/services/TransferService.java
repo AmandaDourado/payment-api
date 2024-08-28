342c776d4dc3ad093dad8db6bdd2120d350b9780
Amanda Dourado <amanda_lopes1998@hotmail.com>
2024-08-28 20:31:11 -0300

    business rule for transfers

--- src/main/java/com/payment/api/services/TransferService.java
+++ src/main/java/com/payment/api/services/TransferService.java
@@ -3,6 +3,7 @@ package com.payment.api.services;
 import com.payment.api.dtos.TransferDTO;
 import com.payment.api.entities.Transfer;
 import com.payment.api.entities.Wallet;
+import com.payment.api.enums.UserType;
 import com.payment.api.repositories.TransferRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,8 +21,27 @@ public class TransferService {
     private WalletService walletService;
 
     public Transfer create(TransferDTO obj) throws Exception {
+        if(obj.getValue() <= 0){
+            throw new IllegalArgumentException("The value must be greater than zero");
+        }
+
+        if(obj.getPayee() == obj.getPayer()) {
+            throw new IllegalArgumentException("The payer and the payee cannot be the same person in a bank transfer");
+        }
+
         Wallet payer = walletService.findById(obj.getPayer());
         Wallet payee = walletService.findById(obj.getPayee());
+
+        if(payer.getUserType().equals(UserType.SHOPKEEPER)) {
+            throw new IllegalArgumentException("Shopkeeper is not authorized to make transfers");
+        }
+
+        if(payer.getBalance() < obj.getValue()) {
+            throw new IllegalArgumentException("The payer has insufficient balance");
+        }
+
+        payer.setBalance(payer.getBalance() - obj.getValue());
+        payee.setBalance(payee.getBalance() + obj.getValue());
 
         Transfer transfer = new Transfer();
         transfer.setValue(obj.getValue());
